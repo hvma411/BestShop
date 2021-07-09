b4a2e537a487e49c1d4dd257a90faee782d5a2eb
hvma411 <kamil.nosek411@gmail.com>
2021-07-09 19:53:18 +0200

    repairs before firebase deploy

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,14 +1,25 @@
 import React, { Component, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import {
-  HashRouter,
-  Route,
-  Switch,
-  NavLink,
+    HashRouter,
+    Route,
+    Switch,
+    NavLink,
 } from 'react-router-dom';
 import { Link } from 'react-scroll'
 
 const HeaderSection = () => {
+
+    const [isMenuActive, setIsMenuActive] = useState(false);
+
+    const handleClick = () => {
+        setIsMenuActive(!isMenuActive);
+    }
+
+    const linkClick = () => {
+        setIsMenuActive(false);
+    }
+
     return (
         <>
             <header>
@@ -17,13 +28,17 @@ const HeaderSection = () => {
                         <div className="logo--box">
                             <div className="logo">BestShop</div>
                         </div>
-                        <nav className="nav--box">
+                        <div onClick={handleClick} className={`hamburger-menu-wrapper ${isMenuActive ? "hamburger-active" : "hamburger-inActive"}`}>
+                            <div className="hamburger-line"></div>
+                            <div className="hamburger-second-line"></div>
+                        </div>
+                        <nav className={`nav--box ${isMenuActive ? "menu-visible" : ""}`}>
                             <ul>
-                                <Link to="home" smooth="true" duration={1000}><li>HOME</li></Link>
-                                <Link to="why-us" smooth="true" duration={1000}><li>WHY US?</li></Link>
-                                <Link to="benefits" smooth="true" duration={1000}><li>BENEFITS</li></Link>
-                                <Link to="prices" smooth="true" duration={1000}><li>PRICES</li></Link>
-                                <Link to="footer" smooth="true" duration={1000}><li>CONTACT</li></Link>
+                                <Link to="home" onClick={linkClick} smooth="true" duration={1000}><li>HOME</li></Link>
+                                <Link to="why-us" onClick={linkClick} smooth="true" duration={1000}><li>WHY US?</li></Link>
+                                <Link to="benefits" onClick={linkClick} smooth="true" duration={1000}><li>BENEFITS</li></Link>
+                                <Link to="prices" onClick={linkClick} smooth="true" duration={1000}><li>PRICES</li></Link>
+                                <Link to="footer" onClick={linkClick} smooth="true" duration={1000}><li>CONTACT</li></Link>
                             </ul>
                         </nav>
                     </div>
